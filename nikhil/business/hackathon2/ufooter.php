094a3679a5328e3f4a4c3dd39faee0068a704d65
Athul S Krishnan <99281249+athulskrish@users.noreply.github.com>
2023-04-18 22:42:31 +0530

    Add files via upload

---
+++ nikhil/business/hackathon2/ufooter.php
@@ -0,0 +1,27 @@
+  <section class="u-clearfix u-palette-1-base u-section-6" id="sec-9453">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-center u-container-style u-group u-group-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <h2 class="u-custom-font u-font-georgia u-text u-text-1">Try this now!</h2>
+            <p class="u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+            <a href="https://nicepage.com/website-mockup" class="u-btn u-button-style u-custom-color-2 u-btn-1">Read More</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/css-templates" target="_blank">
+        <span>CSS Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.best" target="_blank">
+        <span>Best Website Builder</span>
+      </a>. 
+    </section>
+  
+</body></html>
